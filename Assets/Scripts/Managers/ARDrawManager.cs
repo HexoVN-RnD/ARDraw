b213ed40b3a21cd9944029e4c640144dbbddc064
Lê Duy Hiếu <hieu24052002@gmail.com>
2023-11-30 12:00:23 +0700

    update

--- Assets/Scripts/Managers/ARDrawManager.cs
+++ Assets/Scripts/Managers/ARDrawManager.cs
@@ -2,6 +2,7 @@
 using DilmerGames.Core.Singletons;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
 
 [RequireComponent(typeof(ARAnchorManager))]
@@ -14,9 +15,6 @@ public class ARDrawManager : Singleton<ARDrawManager>
     private UnityEvent OnDraw = null;
 
     [SerializeField]
-    private ARAnchorManager anchorManager = null;
-
-    [SerializeField] 
     private Camera arCamera = null;
 
     private List<ARAnchor> anchors = new List<ARAnchor>();
@@ -25,42 +23,62 @@ public class ARDrawManager : Singleton<ARDrawManager>
 
     private bool CanDraw { get; set; }
 
-    void Update ()
+    void Update()
     {
-        #if !UNITY_EDITOR    
+#if !UNITY_EDITOR
         DrawOnTouch();
-        #else
+#else
         DrawOnMouse();
-        #endif
-	}
+#endif
+    }
 
     public void AllowDraw(bool isAllow)
     {
         CanDraw = isAllow;
     }
 
-
     void DrawOnTouch()
     {
-        if(!CanDraw) return;
+        if (!CanDraw) return;
+
+        if (Input.touchCount == 0) return;
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // Check if the touch is over a UI element
+            if (EventSystem.current.currentSelectedGameObject != null)
+            {
+                // If a UI element is selected, ignore the touch
+                return;
+            }
+        }
 
         int tapCount = Input.touchCount > 1 && lineSettings.allowMultiTouch ? Input.touchCount : 1;
 
-        for(int i = 0; i < tapCount; i++)
+        for (int i = 0; i < tapCount; i++)
         {
+            if (i >= Input.touchCount) return;
+
             Touch touch = Input.GetTouch(i);
             Vector3 touchPosition = arCamera.ScreenToWorldPoint(new Vector3(Input.GetTouch(i).position.x, Input.GetTouch(i).position.y, lineSettings.distanceFromCamera));
-            
-            ARDebugManager.Instance.LogInfo($"{touch.fingerId}");
 
-            if(touch.phase == TouchPhase.Began)
+            //ARDebugManager.Instance.LogInfo($"{touch.fingerId}");
+
+            if (touch.phase == TouchPhase.Began)
             {
                 OnDraw?.Invoke();
-                
-                ARAnchor anchor = anchorManager.AddAnchor(new Pose(touchPosition, Quaternion.identity));
-                if (anchor == null) 
+
+                //ARAnchor anchor = anchorManager.AddAnchor(new Pose(touchPosition, Quaternion.identity));
+                var anchorObject = new GameObject("MyARAnchor");
+                anchorObject.transform.position = touchPosition;
+                anchorObject.transform.rotation = Quaternion.identity;
+                ARAnchor anchor = anchorObject.AddComponent<ARAnchor>();
+
+                if (anchor == null)
                     Debug.LogError("Error creating reference point");
-                else 
+                else
                 {
                     anchors.Add(anchor);
                     ARDebugManager.Instance.LogInfo($"Anchor created & total of {anchors.Count} anchor(s)");
@@ -70,11 +88,11 @@ public class ARDrawManager : Singleton<ARDrawManager>
                 Lines.Add(touch.fingerId, line);
                 line.AddNewLineRenderer(transform, anchor, touchPosition);
             }
-            else if(touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+            else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
             {
                 Lines[touch.fingerId].AddPoint(touchPosition);
             }
-            else if(touch.phase == TouchPhase.Ended)
+            else if (touch.phase == TouchPhase.Ended)
             {
                 Lines.Remove(touch.fingerId);
             }
@@ -83,28 +101,34 @@ public class ARDrawManager : Singleton<ARDrawManager>
 
     void DrawOnMouse()
     {
-        if(!CanDraw) return;
+        if (!CanDraw) return;
 
         Vector3 mousePosition = arCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, lineSettings.distanceFromCamera));
 
-        if(Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0))
         {
+            if (EventSystem.current.IsPointerOverGameObject())
+            {
+                // If the touch is over a UI element, ignore it
+                return;
+            }
+
             OnDraw?.Invoke();
 
-            if(Lines.Keys.Count == 0)
+            if (Lines.Keys.Count == 0)
             {
                 ARLine line = new ARLine(lineSettings);
                 Lines.Add(0, line);
                 line.AddNewLineRenderer(transform, null, mousePosition);
             }
-            else 
+            else
             {
                 Lines[0].AddPoint(mousePosition);
             }
         }
-        else if(Input.GetMouseButtonUp(0))
+        else if (Input.GetMouseButtonUp(0))
         {
-            Lines.Remove(0);   
+            Lines.Remove(0);
         }
     }
 
@@ -121,5 +145,6 @@ public class ARDrawManager : Singleton<ARDrawManager>
             LineRenderer line = currentLine.GetComponent<LineRenderer>();
             Destroy(currentLine);
         }
+        //Lines.Clear();
     }
 }
